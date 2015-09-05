0f0f73a1af861e7d97ad0e95b343f30b036e53f6
Daniel Atkinson <hourd.tasa@gmail.com>
2015-09-05 23:15:02 +0100

    Added rustful as a dependancy for REST api functionality Unwritten test as unsure how to test a REST api

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+//Include macros to be able to use `insert_routes!`.
+#[macro_use]
+extern crate rustful;
+
+use std::error::Error;
+
+use rustful::{Server, Context, Response, TreeRouter, Handler};
+
+fn say_hello(context: Context, response: Response) {
+    //Get the value of the path variable `:person`, from below.
+    let person = match context.variables.get("person") {
+        Some(name) => name,
+        None => "stranger".into()
+    };
+
+    //Use the name from the path variable to say hello.
+    response.send(format!("Hello, {}!", person));
 }
 
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+//Dodge an ICE, related to functions as handlers.
+struct HandlerFn(fn(Context, Response));
+
+impl Handler for HandlerFn {
+    fn handle_request(&self, context: Context, response: Response) {
+        self.0(context, response);
+    }
+}
+
+fn main() {
+    //Build and run the server.
+    let server_result = Server {
+        //Turn a port number into an IPV4 host address (0.0.0.0:8080 in this case).
+        host: 8080.into(),
+
+        //Create a TreeRouter and fill it with handlers.
+        handlers: insert_routes!{
+            TreeRouter::new() => {
+                //Handle requests for root...
+                Get: HandlerFn(say_hello),
+
+                //...and one level below.
+                //`:person` is a path variable and it will be accessible in the handler.
+                ":person" => Get: HandlerFn(say_hello)
+            }
+        },
+
+        //Use default values for everything else.
+        ..Server::default()
+    }.run();
+
+    match server_result {
+        Ok(_server) => {},
+        Err(e) => println!("could not start server: {}", e.description())
+    }
 }
 
 #[test]
-fn it_works() {
-    assert_eq!(4, add_two(2));
+fn saying_hello() {
+    //Not really sure how to test this
 }
+
